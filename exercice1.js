052833cc02c5d6679c59e274a70c49bc365b4394
Clems-web <clement.servais1@gmail.com>
2020-12-14 15:38:09 +0100

    Pas finito !

--- exercice1.js
+++ exercice1.js
@@ -13,6 +13,29 @@ let articles = [
 // Votre code à partir d'ici :
 
 
+let liste = document.getElementById("listeCourse");
+let champ = document.getElementById("user-text");
+let buttonAdd = document.getElementById("button-add");
+
+buttonAdd.addEventListener("click", function () {
+    
+})
+
+for (x = 0; x < articles.length; x++) {
+    let newPuce = document.createElement("li");
+
+    newPuce.innerHTML = articles[x];
+    let bouton = document.createElement("button");
+
+    bouton.innerHTML = "hide";
+    bouton.style.marginLeft = "2rem";
+    newPuce.append(bouton);
+    liste.append(newPuce);
+    bouton.addEventListener("click", function () {
+        newPuce.innerHTML = "";
+    })
+}
+
 
 
 
