92f1a4ca20b6ad0e94982688192c482880ebd5ed
Clems-web <clement.servais1@gmail.com>
2020-12-15 09:49:25 +0100

    Partie IV terminé : ajout et supp des entrées dans l'input

--- exercice1.js
+++ exercice1.js
@@ -16,14 +16,39 @@ let articles = [
 let liste = document.getElementById("listeCourse");
 let champ = document.getElementById("user-text");
 let buttonAdd = document.getElementById("button-add");
+let buttonDel = document.getElementById("button-supp");
+
+/* Add an item to your shopping list */
 
 buttonAdd.addEventListener("click", function () {
+    let newPuce = document.createElement("li");
+    newPuce.innerHTML = champ.value;
+    let bouton = document.createElement("button");
+
+    bouton.innerHTML = "hide";
+    bouton.style.marginLeft = "2rem";
+    newPuce.append(bouton);
+    liste.append(newPuce);
+
+    /* Adding a button to hide one of the item */
     
+    bouton.addEventListener("click", function () {
+        newPuce.innerHTML = "";
+    })
+
+
 })
+
+/* remove last item of your shopping list */
+
+buttonDel.addEventListener("click", function () {
+    liste.lastChild.remove();
+})
+
+/* add property of "articles" in the shopping list */
 
 for (x = 0; x < articles.length; x++) {
     let newPuce = document.createElement("li");
-
     newPuce.innerHTML = articles[x];
     let bouton = document.createElement("button");
 
